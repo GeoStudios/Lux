6abc807fb3d48b501dcb33c054f24b6580b50429
Zombi <zombeifedgaming@outlook.com>
2023-05-07 16:01:33 -0500

    Added Code Editor Area And Added Modular Imports.json requirement for all lua type packages

--- plugins/obscurus/sub/xpack.js
+++ plugins/obscurus/sub/xpack.js
@@ -6,19 +6,35 @@ if (config.packs != []){
     loop(config.packs.length, (i)=>{
         if (LoadPlugJson(config.packs[i].folder).apiVer == apiVer || CbaseSupported.includes(LoadPlugJson(config.packs[i].folder).apiVer)){
             
+            const imports = loadFile(filepath("../../"+config.packs[i].folder+"/imports.json")).imports
+
+            let Imports = new Array(imports.length)
+            loop(imports.length, (q)=>{
+                const module = modules.fs.readFileSync(getFilePath("../../"+imports[q], __filename))
+                Imports.push(module)
+            })
+
             const os = modules.fs.readFileSync(getFilePath("../lib/os.lua", __filename))
             const lux = modules.fs.readFileSync(getFilePath("../lib/lux.lua", __filename))
             var LuaVM = modules.luavmjs;
             var l = new LuaVM.Lua.State();
             var file = modules.fs.readFileSync(getFilePath("../../"+config.packs[i].folder+"/pack.lua", __filename))
+            // var lib = `
+            // filename = [[${__filename}]]
+            // path = js.global:require("path")
+            // fs = js.global:require("fs")
+            // child_process = js.global:require("child_process")
+            
+            // ${os}
+            // ${lux}
+            // `
             var lib = `
             filename = [[${__filename}]]
             path = js.global:require("path")
             fs = js.global:require("fs")
             child_process = js.global:require("child_process")
             
-            ${os}
-            ${lux}
+            ${Imports.join("\n")}
             `
             var whole =`${lib}
 
